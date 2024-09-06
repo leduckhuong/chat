58ebda4bcac140775a1d8cf1e962112e1660df47
leduckhuong <leduckhuong@kali.kali.org>
2024-09-06 09:33:12 +0700

    add files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: 'class',
-    content: ["./src/**/*.{ejs,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-}
